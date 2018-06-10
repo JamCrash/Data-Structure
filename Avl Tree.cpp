61d2bea4847d043130cab0facbed4ab507c807c2
JamCrash <39126346+JamCrash@users.noreply.github.com>
2018-06-10 17:15:38 +0800

    Update Avl Tree.cpp

--- Avl Tree.cpp
+++ Avl Tree.cpp
@@ -1,5 +1,6 @@
 #include "AvlNode.h"
 #include<stdexcept>
+#include<iostream>
 
 
 AvlNode::AvlNode()
@@ -9,7 +10,7 @@ AvlNode::AvlNode()
 }
 
 void 
-AvlNode::MakeEmpty(AvlTree T)
+AvlNode::MakeEmpty(AvlTree& T)
 {
 	if (T) {
 		MakeEmpty(T->Left);
@@ -19,7 +20,7 @@ AvlNode::MakeEmpty(AvlTree T)
 }
 
 Position
-AvlNode::Find(int val, AvlTree T)
+AvlNode::Find(int val, AvlTree& T)
 {
 	if (T == nullptr)
 		return nullptr;
@@ -33,38 +34,42 @@ AvlNode::Find(int val, AvlTree T)
 }
 
 Position
-AvlNode::FindMin(AvlTree T)
+AvlNode::FindMin(AvlTree& T)
 {
-	if (T == nullptr)
-		throw std::runtime_error("Empty AvlTree");
-	else if (T->Left)
-		FindMin(T->Left);
-	else
-		return T;
+	if (T)
+		if (T->Left)
+			return FindMin(T->Left);
+		else
+			return T;
+	return nullptr;
 }
 
 Position
-AvlNode::FindMax(AvlTree T)
+AvlNode::FindMax(AvlTree& T)
 {
-	if (T == nullptr)
-		throw std::runtime_error("Empty Avltree");
-	else if (T->Right)
-		FindMax(T->Right);
-	else
-		return T;
+	if (T)
+		if (T->Right)
+			return FindMax(T->Right);
+		else
+			return T;
+	return nullptr;
 }
 
 AvlTree
-AvlNode::Insert(int val, AvlTree T)
+AvlNode::Insert(int val, AvlTree& T)
 {
 	if (T == nullptr) {
 		T = new AvlNode();
 		if (T == nullptr)
 			throw std::runtime_error("Out of space!");
+		else {
+			T->Element = val; T->height = 0;
+			T->Left = T->Right = nullptr;
+		}
 	}
 	else if (val < T->Element) {
 		T->Left = Insert(val, T->Left);
-		if (T->Left->height - T->Right->height == 2)
+		if (Height(T->Left) - Height(T->Right) == 2)
 			if (val < T->Left->Element)
 				T = SingleRotateWithLeft(T);
 			else
@@ -72,7 +77,7 @@ AvlNode::Insert(int val, AvlTree T)
 	}
 	else if (val > T->Element) {
 		T->Right = Insert(val, T->Right);
-		if (T->Right->height - T->Left->Element == 2)
+		if (Height(T->Right) - Height(T->Left) == 2)
 			if (val > T->Right->Element)
 				T = SingleRotateWithRight(T);
 			else
@@ -84,10 +89,77 @@ AvlNode::Insert(int val, AvlTree T)
 	return T;
 }
 
-AvlTree
-AvlNode::Delete(int val, AvlTree T)
+void
+AvlNode::Delete(int val, AvlTree& T)
 {
 	if (T == nullptr)
-		throw std::runtime_error("Empty AvlTree");
-	else if(val < T)
+		throw std::runtime_error("The value is not exist");
+	if (val < T->Element) {
+		Delete(val, T->Left);
+		T->height = MaxHeight(T->Left, T->Right) + 1;
+		if (Height(T->Right) - Height(T->Left) == 2)
+			T = SingleRotateWithRight(T);
+	}
+	else if (val > T->Element) {
+		Delete(val, T->Right);
+		T->height = MaxHeight(T->Left, T->Right) + 1;
+		if (Height(T->Left) - Height(T->Right) == 2)
+			T = SingleRotateWithLeft(T);
+	}
+	/* Else T->Element == val, then we'll delete it */
+	else {
+		if (T->Left && T->Right) {
+			Position TmpCell;
+
+			if (Height(T->Left) == Height(T->Right)) {
+				TmpCell = FindMax(T->Left);
+				T->Element = TmpCell->Element;
+				Delete(T->Element, T->Left);
+			}
+			else {
+				if (Height(T->Left) > Height(T->Right)) {
+					TmpCell = FindMax(T->Left);
+					T->Element = TmpCell->Element;
+					Delete(T->Element, T->Left);
+				}
+				else {
+					TmpCell = FindMin(T->Right);
+					T->Element = TmpCell->Element;
+					Delete(T->Element, T->Right);
+				}
+				T->height -= 1;
+			}
+		}
+		else if (T->Left || T->Right) {
+			Position TmpCell;
+
+			if (T->Left) {
+				TmpCell = T->Left;
+				T->Element = TmpCell->Element;
+				T->height = 0;
+				T->Left = nullptr;
+			}
+			else {
+				TmpCell = T->Right;
+				T->Element = TmpCell->Element;
+				T->height = 0;
+				T->Right = nullptr;
+			}
+			delete TmpCell;
+		}
+		else {
+			delete T;
+			T = nullptr;
+		}	
+	}
+}
+
+void
+Preorder_Traversal(const AvlTree T)
+{
+	if (T != nullptr) {
+		std::cout << T->Element << " ";
+		Preorder_Traversal(T->Left);
+		Preorder_Traversal(T->Right);
+	}
 }
