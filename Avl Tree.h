e4e0ce6b647ca59cb5e13def038a8bcfef68219f
JamCrash <39126346+JamCrash@users.noreply.github.com>
2018-06-10 17:14:36 +0800

    Update Avl Tree.h

--- Avl Tree.h
+++ Avl Tree.h
@@ -6,16 +6,17 @@ typedef AvlNode* AvlTree;
 
 class AvlNode
 {
+	friend void Preorder_Traversal(const AvlTree T);
 public://构造函数
 	AvlNode();
 	~AvlNode() = default;
 public://成员操作
-	void MakeEmpty(AvlTree T);
-	Position Find(int val, AvlTree T);
-	Position FindMin(AvlTree T);
-	Position FindMax(AvlTree T);
-	AvlTree Insert(int val, AvlTree T);
-	AvlTree Delete(int val, AvlTree T);
+	void MakeEmpty(AvlTree& T);
+	Position Find(int val, AvlTree& T);
+	Position FindMin(AvlTree& T);
+	Position FindMax(AvlTree& T);
+	AvlTree Insert(int val, AvlTree& T);
+	void Delete(int val, AvlTree& T);
 	int get_val() const { return Element; }
 	int get_height() const { return height; }
 private://私有成员
@@ -24,10 +25,11 @@ private://私有成员
 	AvlTree Left;
 	AvlTree Right;
 private://私有函数
-	int Height(Position P) const;
+	static int Height(Position P);
 	static int MaxHeight(AvlTree TreeFirst, AvlTree TreeSecond);
 	static Position SingleRotateWithLeft(Position P);
 	static Position SingleRotateWithRight(Position P);
 	static Position DoubleRotateWithLeft(Position P);
 	static Position DoubleRotateWithRight(Position P);
 };
+
